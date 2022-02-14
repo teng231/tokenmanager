64b4f8fbcdfbdb31d974045eb4b8768f849efe2f
TeNg <manhte231@gmail.com>
2022-02-14 11:09:43 +0700

    update test case

--- hmac_token_test.go
+++ hmac_token_test.go
@@ -12,34 +12,47 @@ func TestGenHmacToken(t *testing.T) {
 	if err != nil {
 		t.Fail()
 	}
-	tk, err := mgr.GenerateHmacToken(Claims{UserId: 1}, 30*time.Second)
+	_tk, err := mgr.GenerateHmacToken(Claims{UserId: 1}, 30*time.Second)
 	if err != nil {
 		log.Print(err)
 		t.Fail()
 	}
-	log.Println(tk)
+	log.Println(_tk)
 }
 
 func TestParseHmacToken(t *testing.T) {
-	tk := `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2MzIxMDg3MTcsImlhdCI6MTYzMjEwODY4NywicGF5bG9hZCI6eyJ1c2VyX2lkIjoxLCJyb2xlX2lkIjowfX0.R2mcIpb03PuGSTXLtWLEgDdzuJvBkE-F7ukfALlUtKk`
 	mgr, err := CreateHMACToken("1234@sdfdfda}")
+	if err != nil {
+		log.Print(1, err)
+		t.Fail()
+	}
+	_tk, err := mgr.GenerateHmacToken(Claims{UserId: 1}, 30*time.Second)
 	if err != nil {
 		log.Print(err)
 		t.Fail()
 	}
-	extractor, err := mgr.ParseHmacToken(tk)
+	extractor, err := mgr.ParseHmacToken(_tk)
 	if err != nil {
-		log.Print(err)
+		log.Print(2, err)
 		t.Fail()
 	}
 	bin, err := json.Marshal(extractor)
 	if err != nil {
+		log.Print(3, err)
 		t.Fail()
 	}
 	claim := &Claims{}
 	json.Unmarshal(bin, claim)
 	log.Println(claim)
 	if claim.UserId != 1 {
+		t.Fail()
+	}
+
+	// -- with token expired
+	expiredTk := `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2MzIxMDg3MTcsImlhdCI6MTYzMjEwODY4NywicGF5bG9hZCI6eyJ1c2VyX2lkIjoxLCJyb2xlX2lkIjowfX0.R2mcIpb03PuGSTXLtWLEgDdzuJvBkE-F7ukfALlUtKk`
+	extractor, err = mgr.ParseHmacToken(expiredTk)
+	if err == nil {
+		log.Print(extractor)
 		t.Fail()
 	}
 }
--- rsa_token_test.go
+++ rsa_token_test.go
@@ -25,8 +25,12 @@ func TestGenRSAToken(t *testing.T) {
 }
 
 func TestParseRSAToken(t *testing.T) {
-	tk := `eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2MzIxMTEzOTYsImlhdCI6MTYzMjEwOTU5NiwicGF5bG9hZCI6eyJ1c2VyX2lkIjoxLCJyb2xlX2lkIjowfX0.Lumm89uR8X3NeKCJozzqdYf8WHJr1g-YjBFBA2_rxjRn5Uf0-nZGI0XisOOqcnXmIdMTrl1N3R4Ws7bsRrHwW_scQL3HRpK-_6fGOPzdv1IlN-JprWUeHvlbTqybPSyPwpkiEPi9n3vqSjeh-YOMw03F9xtXsI6Mpz5TVbjEEsRO06W5GZCx9FLxIckVodyawFdOnFsMYyFM44hIJMNex260VOQmPkBTUqunT966vYpQXOfkEcJaQQ4rfrarhhbbAEwXkpAIHksHOwKvsX3YtrYrT2QQY2Hhh7ZASaNWNs0KPQsVTrxR3mnz-tljz2LnXB_FGg8cdK3gfLzdHpYeK91C3WkTkhJNusNzgSlw2rtDFbNnaAgkskCjyi1BSKklI_UYWfu_U87OnGphuXwcuEOh_27827BhKfjDCC57EeKlgrqZPRCjExIHmMyw5YwMUM940cbkEJrzIA2qipfEFO-mfLgRrv2U7meuEo0MSlhjXfp9GQRQ1PMVnq36eU5boaAitGF6Gezw3vajsKi5_jcLlJyYLuFHSj79qv9iOG3yqbScB31tbDmmqP356Gj48kHMI_pkBESVAvqoIRceTch-CsTDh2VpwFiywIhKSxQ-50S5QoGRk0FN8LwUM0Xh737Dw95AlqV4JRXs_Y6PrCp-Jb-RbZ4ZVFsHTiqCqGI`
-	mgr, err := CreateRSAToken("./test/jwtRS256.key.pub", "")
+
+	mgr, err := CreateRSAToken("./test/jwtRS256.key.pub", "./test/jwtRS256.key")
+	if err != nil {
+		t.Fail()
+	}
+	tk, err := mgr.GenerateToken(Claims{UserId: 1}, 30*time.Minute)
 	if err != nil {
 		t.Fail()
 	}
@@ -45,4 +49,13 @@ func TestParseRSAToken(t *testing.T) {
 	if claim.UserId != 1 {
 		t.Fail()
 	}
+	// with token expired
+
+	tkExpired := `eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2MzIxMTEzOTYsImlhdCI6MTYzMjEwOTU5NiwicGF5bG9hZCI6eyJ1c2VyX2lkIjoxLCJyb2xlX2lkIjowfX0.Lumm89uR8X3NeKCJozzqdYf8WHJr1g-YjBFBA2_rxjRn5Uf0-nZGI0XisOOqcnXmIdMTrl1N3R4Ws7bsRrHwW_scQL3HRpK-_6fGOPzdv1IlN-JprWUeHvlbTqybPSyPwpkiEPi9n3vqSjeh-YOMw03F9xtXsI6Mpz5TVbjEEsRO06W5GZCx9FLxIckVodyawFdOnFsMYyFM44hIJMNex260VOQmPkBTUqunT966vYpQXOfkEcJaQQ4rfrarhhbbAEwXkpAIHksHOwKvsX3YtrYrT2QQY2Hhh7ZASaNWNs0KPQsVTrxR3mnz-tljz2LnXB_FGg8cdK3gfLzdHpYeK91C3WkTkhJNusNzgSlw2rtDFbNnaAgkskCjyi1BSKklI_UYWfu_U87OnGphuXwcuEOh_27827BhKfjDCC57EeKlgrqZPRCjExIHmMyw5YwMUM940cbkEJrzIA2qipfEFO-mfLgRrv2U7meuEo0MSlhjXfp9GQRQ1PMVnq36eU5boaAitGF6Gezw3vajsKi5_jcLlJyYLuFHSj79qv9iOG3yqbScB31tbDmmqP356Gj48kHMI_pkBESVAvqoIRceTch-CsTDh2VpwFiywIhKSxQ-50S5QoGRk0FN8LwUM0Xh737Dw95AlqV4JRXs_Y6PrCp-Jb-RbZ4ZVFsHTiqCqGI`
+	extractor, err = mgr.ParseToken(tkExpired)
+	if err == nil {
+		log.Print(extractor)
+		t.Fail()
+	}
+
 }
